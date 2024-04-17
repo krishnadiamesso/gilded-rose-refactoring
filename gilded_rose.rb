9c8897bc5ac34590fa257e7242f8960e75fb3332
Krishna Diamesso <krishnadiamesso@gmail.com>
2024-04-17 05:46:55 -0400

    updated update_sulfuras

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -18,52 +18,6 @@ class GildedRose
     item.quality = 0 if item.sell_in < 0
   end
 
-  def update_sulfuras(item)
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      if item.quality > 0
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.quality = item.quality - 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != "Backstage passes to a TAFKAL80ETC concert"
-          if item.quality > 0
-            if item.name != "Sulfuras, Hand of Ragnaros"
-              item.quality = item.quality - 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-      end
-    end
-  end
-
   def update_normal_item(item)
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
@@ -118,8 +72,6 @@ class GildedRose
         update_aged_brie(item)
       when "Backstage passes to a TAFKAL80ETC concert"
         update_backstage_passes(item)
-      when "Sulfuras, Hand of Ragnaros"
-        update_sulfuras(item)
       else
         update_normal_item(item)
       end
