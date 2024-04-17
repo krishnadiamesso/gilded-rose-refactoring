d5db38bdddf921950bd5d783e9aa28160131532b
Krishna Diamesso <krishnadiamesso@gmail.com>
2024-04-17 05:32:49 -0400

    Initial commit

---
+++ gilded_rose.rb
@@ -0,0 +1,96 @@
+class GildedRose
+
+  def initialize(items)
+    @items = items
+  end
+
+  def update_aged_brie(item)
+    # code here
+  end
+
+  def update_backstage_passes(item)
+    # code here
+  end
+
+  def update_sulfuras(item)
+    # code here
+  end
+
+  def update_normal_item(item)
+    # code here
+  end
+
+  def update_quality()
+    @items.each do |item|
+
+      case item.name
+      when "Aged Brie"
+        update_aged_brie(item)
+      when "Backstage passes to a TAFKAL80ETC concert"
+        update_backstage_passes(item)
+      when "Sulfuras, Hand of Ragnaros"
+        update_sulfuras(item)
+      else
+        update_normal_item(item)
+      end
+
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+          end
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+          end
+        end
+      end
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              if item.name != "Sulfuras, Hand of Ragnaros"
+                item.quality = item.quality - 1
+              end
+            end
+          else
+            item.quality = item.quality - item.quality
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
+    end
+  end
+end
+
+class Item
+  attr_accessor :name, :sell_in, :quality
+
+  def initialize(name, sell_in, quality)
+    @name = name
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def to_s()
+    "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
